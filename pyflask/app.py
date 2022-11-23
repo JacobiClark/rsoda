c0105c857f008187439a4094a23913701ef33d84
Jacob Clark <jacobcs@zoho.com>
2022-11-22 19:40:10 -0800

    index on main: 93d918d feat: added layout component with drawer

---
+++ pyflask/app.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from flask import Flask, request, jsonify
+import time
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=["GET"])
+def hello():
+    return "Server active!"
+
+@app.route('/echo', methods=["POST"])
+def echo():
+    try:
+        data = request.json
+        print("Server: ", data["key"])
+        return "Hello World from Server!"
+    except Exception as e:
+        raise e
+
+# :5000 is the flask default port.
+# You can change it to something else if you would like.
+if __name__ == '__main__':
+    app.run(host='localhost', port=1212)
